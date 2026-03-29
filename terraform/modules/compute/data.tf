fc46895508e909ec4c3b40e2b5222797bbcea944
hemanth <hemanthtadikonda29@gmail.com>
2026-03-29 16:10:52 +0530

    Terraform based deployment for MLops Model

--- terraform/modules/compute/data.tf
+++ terraform/modules/compute/data.tf
@@ -2,18 +2,3 @@ data "aws_route53_zone" "this" {
   name         = var.domain_name
   private_zone = false
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
