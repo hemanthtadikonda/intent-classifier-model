b1de0f8cb019d09f6e2d4900c0fbf6ca3be4ce6e
hemanth <hemanthtadikonda29@gmail.com>
2026-03-29 16:04:10 +0530

    Terraform based deployment for MLops Model

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -79,7 +79,7 @@ resource "aws_launch_template" "lt" {
   instance_type = var.instance_type
   key_name      = var.key_name
 
-  user_data = base64encode(file("{path.module}/userdata.sh"))
+  user_data = base64encode(file("${path.module}/userdata.sh"))
 
   network_interfaces {
     security_groups = [aws_security_group.ec2_sg.id]
